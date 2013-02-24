bcf7c18eb730fe79d688237390198a8e560a718d
Kiran Kulkarni <kk.questworld@gmail.com>
2013-02-24 19:15:02 +0530

    Preparation for clojars deployment

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-id3 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.clojars.kiran/clj-id3 "0.1.0"
+  :description "A Clojure library designed to parse ID3v2 tags from mp3 files."
+  :url "https://github.com/kirankulkarni/clj-id3"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
